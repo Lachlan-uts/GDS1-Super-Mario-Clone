fbc4a38c9be9a5591e9bcccef334c80f0a9a05b9
Lachlan <Lachlan.d.wood@student.uts.edu.au>
2018-03-30 03:32:31 +1100

    disabled the osx build

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -12,15 +12,15 @@ echo "Attempting to build $project for Windows"
   -buildWindowsPlayer "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" \
   -quit
 
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
-  -quit
+# echo "Attempting to build $project for OS X"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  # -batchmode \
+  # -nographics \
+  # -silent-crashes \
+  # -logFile $(pwd)/unity.log \
+  # -projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+  # -buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
+  # -quit
 
 # echo "Attempting to build $project for Linux"
 # /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -38,5 +38,5 @@ cat $(pwd)/unity.log
 
 echo 'Attempting to zip builds'
 # zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+# zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
 zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
