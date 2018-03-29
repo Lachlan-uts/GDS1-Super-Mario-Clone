6be3a0d988a80c045980f16388f6f3e498e34a56
Lachlan <Lachlan.d.wood@student.uts.edu.au>
2018-03-30 03:25:28 +1100

    disabled the linux build for now.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -22,21 +22,21 @@ echo "Attempting to build $project for OS X"
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
   -quit
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/${UNITYCI_PROJECT_NAME}.exe" \
-  -quit
+# echo "Attempting to build $project for Linux"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  # -batchmode \
+  # -nographics \
+  # -silent-crashes \
+  # -logFile $(pwd)/unity.log \
+  # -projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+  # -buildLinuxUniversalPlayer "$(pwd)/Build/linux/${UNITYCI_PROJECT_NAME}.exe" \
+  # -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
 
 
 echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+# zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
 zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
 zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
