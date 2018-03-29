8e00b73a4bce9b165ea7e20ea8243732661d5068
mordakai95 <wgp_1995@live.com>
2018-03-29 18:15:00 +1100

    29/03/2018 Powerups
     - Implemented powerups

--- Super Mario Clone/Assets/Scripts/PlayerBehaviourScript.cs
+++ Super Mario Clone/Assets/Scripts/PlayerBehaviourScript.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class PlayerBehaviourScript : MonoBehaviour {
 
+	// serialized private variables
+	[SerializeField]
+	private GameObject fireball;
 
+	// private variables
 	private Rigidbody2D playerRB2D;
+
+	private int healthState;
+
+	private int livesCount;
+
+	private bool facingRight;
 
 	// Use this for initialization
 	void Start () {
 		playerRB2D = GetComponent<Rigidbody2D> ();
+		healthState = 0; // In short, take a single hit, death ensues
+		livesCount = 3;
+		facingRight = true;
 	}
 	
 	// Update is called once per frame
@@ -19,8 +32,43 @@ public class PlayerBehaviourScript : MonoBehaviour {
 
 	void FixedUpdate() {
 		//A simple test to help test if the camera will move with the player object.
-		if (Input.anyKeyDown) {
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
 			playerRB2D.AddForce (Vector2.right * 4,ForceMode2D.Impulse);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			Debug.Log ("Z");
+			if (healthState >= 2) {
+				CreateFireball ();
+				Debug.Log ("Fire ball");
+			}
+		}
+	}
+
+	void CreateFireball() {
+		GameObject newFireball = Instantiate (fireball, gameObject.transform.position, Quaternion.identity) as GameObject;
+		if (facingRight) {
+			newFireball.GetComponent<Rigidbody2D> ().AddForce (Vector2.right * 3.0f, ForceMode2D.Impulse);
+		} else {
+			newFireball.GetComponent<Rigidbody2D> ().AddForce (Vector2.left * 3.0f, ForceMode2D.Impulse);
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Powerup") { // If the player comes into contact with a powerup
+			Debug.Log("CollisionWithPowerup");
+			if (healthState == 0 && other.gameObject.GetComponent<PowerupBehaviourScript> ().typeOfPower == 0) {
+				healthState++;
+				Debug.Log ("Mushroom");
+			} else if (healthState < 2 && other.gameObject.GetComponent<PowerupBehaviourScript> ().typeOfPower == 1) {
+				healthState = 2;
+				Debug.Log ("Fire Flower");
+			} else if (other.gameObject.GetComponent<PowerupBehaviourScript> ().typeOfPower == 2) {
+				livesCount++;
+				Debug.Log ("1-UP");
+			}
+
 		}
 	}
 }
