5c04a46570066318bd246b50716b6342086862c7
Rowena Budiharto <11981673@student.uts.edu.au>
2018-03-29 14:44:59 +1100

    Testing Push

---
+++ Super Mario Clone/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour {
+
+    public GameManager gameManager;
+    //public string deathMenu;
+	// Use this for initialization
+	void Start () {
+        gameManager = FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            //SceneManager.LoadScene(deathMenu);
+            gameManager.RespawnPlayer();
+        }
+    }
+}
