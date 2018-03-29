16e560e82f0203cca0a648597e4e7963e55e1e21
Jacob Harris <jacob.o.harris@student.uts.edu.au>
2018-03-29 11:57:20 +1100

    Added jumping with higher jump when jump key is held down

--- Super Mario Clone/Assets/Scripts/PlayerBehaviourScript.cs
+++ Super Mario Clone/Assets/Scripts/PlayerBehaviourScript.cs
@@ -5,24 +5,61 @@ using UnityEngine;
 public class PlayerBehaviourScript : MonoBehaviour {
 
 	public float speed;
+	public float jumpForce;
+	public float jumpTime;
+	public float jumpTimeCounter;
+	public bool stoppedJumping;
+
+	public bool grounded;
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+	float groundRadius = 0.2f;
 
 	private Rigidbody2D playerRB2D;
 
 	// Use this for initialization
 	void Start () {
+
 		playerRB2D = GetComponent<Rigidbody2D> ();
+		jumpTimeCounter = jumpTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+		if(grounded){
+			jumpTimeCounter = jumpTime;
+		}
+
+		if(Input.GetMouseButtonDown(0)){
+			if(grounded){
+				playerRB2D.velocity = new Vector2(playerRB2D.velocity.x, jumpForce);
+				stoppedJumping = false;
+			}
+		}
+
+		if(Input.GetMouseButtonUp(0)){
+			jumpTimeCounter = 0;
+			stoppedJumping = true;
+		}
+
+
 
 	}
 
 	void FixedUpdate() {
 
+		//basic horizontal movement with a public speed variable
 		float move = Input.GetAxis("Horizontal");
 		playerRB2D.velocity = new Vector2(move * speed, playerRB2D.velocity.y);
-		
-	}
+
+		if((Input.GetMouseButton(0)) && !stoppedJumping)  {
+            if(jumpTimeCounter > 0){
+                playerRB2D.velocity = new Vector2 (playerRB2D.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+    }
 }
