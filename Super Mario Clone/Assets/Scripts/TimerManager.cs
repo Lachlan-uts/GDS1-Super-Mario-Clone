3561f96f1f9e2f0967fccc218296b22eaf3c6895
Rowena Budiharto <11981673@student.uts.edu.au>
2018-04-01 14:09:45 +1000

    20 Minute Countdown and UI working

---
+++ Super Mario Clone/Assets/Scripts/TimerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimerManager : MonoBehaviour {
+	public Text timerText;
+	private float time = 1200;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCountdownTimer ();
+	}
+
+	void StartCountdownTimer()
+	{
+		if (timerText != null) {
+			time = 1200;
+			timerText.text = "Time Left: 20:00:000";
+			InvokeRepeating ("UpdateTimer", 0.0f, 0.01667f);
+		}
+
+	}
+
+	void UpdateTimer()
+	{
+		if(timerText!= null)
+		{time -= Time.deltaTime;
+			string minutes = Mathf.Floor (time / 60).ToString ("00");
+			string seconds = (time % 60).ToString("00");
+				string fraction = ((time * 100) % 100).ToString("000");
+				timerText.text = "Time Left: " + minutes + ":" + seconds + ":" + fraction;
+		}
+				}
+
+
+}
