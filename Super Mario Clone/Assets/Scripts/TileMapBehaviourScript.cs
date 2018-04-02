a9d0a9f0a1565f326d70d979d8da3e83c7ac9db4
Lachlan <lachlan.d.wood@student.uts.edu.au>
2018-04-02 15:17:10 +1000

    Performed initial Tilemap work
    There is now a ground tilemap, a pit tilemap and work done for custom, prefab brushes. This means that as soon as the relevent block prefabs are made, they can be painted into the grid.

---
+++ Super Mario Clone/Assets/Scripts/TileMapBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapBehaviourScript : MonoBehaviour {
+
+	Tilemap tilemap;
+
+	// Use this for initialization
+	void Start () {
+		tilemap = this.GetComponent<Tilemap> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		Debug.Log ("this is " +this.name);
+		ContactPoint2D[] contactP = new ContactPoint2D[1];
+		collision.GetContacts(contactP);
+
+		if (collision.gameObject.tag == "Player") {
+			//tilemap.GetTile
+		}
+	}
+}
