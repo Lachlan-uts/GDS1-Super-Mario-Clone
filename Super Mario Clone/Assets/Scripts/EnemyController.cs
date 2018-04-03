6beed3f77fb393acf2e858121bc6faa18d9004c4
Jacob Harris <jacob.o.harris@student.uts.edu.au>
2018-04-03 11:48:12 +1000

    added a squashcheck and made a new enemy prefab. Have commented out much of the enemy controller script while it isnt being used

--- Super Mario Clone/Assets/Scripts/EnemyController.cs
+++ Super Mario Clone/Assets/Scripts/EnemyController.cs
@@ -7,44 +7,60 @@ public class EnemyController : MonoBehaviour {
 	public float moveSpeed;
 	private bool moveRight;
 
-	public Transform wallCheck;
-	private float wallCheckRadius = 0.1f;
-	public LayerMask whatIsWall;
-	private bool hitWall;
+	public bool squash;
+	public Transform squashCheck;
+	private float squashCheckRadius = .2f;
+	public LayerMask whatIsPlayer;
+	
 
-	public bool grounded;
-	public Transform groundCheck;
-	public LayerMask whatIsGround;
-	float groundRadius = 0.1f;
+	// public Transform wallCheck;
+	// private float wallCheckRadius = 0.1f;
+	// public LayerMask whatIsWall;
+	// private bool hitWall;
+
+	// public bool grounded;
+	// public Transform groundCheck;
+	// public LayerMask whatIsGround;
+	// float groundRadius = 0.1f;
+
+	void OnStart(){
+
+		squash = false;
+	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.name == "Player") {
+		if (other.name == "Player" && !squash){
 			Debug.Log ("If Player is Small, Player Dies, if Player is Super lose a power up");
 			Destroy (other.gameObject);
 			//Have a manager to manage the respawn of the player
 			//gameManager.RespawnPlayer ();
 		}
+		if (other.name == "Player" && squash){
+			Destroy(gameObject);
+		}
 	}
 
 	void Update () {
 
-		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		squash = Physics2D.OverlapCircle(squashCheck.position, squashCheckRadius, whatIsPlayer);
 
-		hitWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+		//grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 
-		if (hitWall)
-			moveRight = !moveRight;
+		// hitWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
 
-		if (moveRight)
-		{
-			transform.localScale = new Vector3(-1f, 1f, 1f);
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		}
-		else
-		{
-			transform.localScale = new Vector3(1f, 1f, 1f);
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		}
+		// if (hitWall)
+		// 	moveRight = !moveRight;
+
+		// if (moveRight)
+		// {
+		// 	transform.localScale = new Vector3(-1f, 1f, 1f);
+		// 	GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		// }
+		// else
+		// {
+		// 	transform.localScale = new Vector3(1f, 1f, 1f);
+		// 	GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		// }
 	}
 }
